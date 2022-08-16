2a21bf6da8c66b3e6486f0d1b49a9bd8a895aa02
Susan-Sharma <susansharma227@gmail.com>
2022-08-16 23:10:51 +0545

    read address

---
+++ CSharpBasics/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Hello, What is your address?");
+var address = Console.ReadLine();
+Console.WriteLine($"Hello I Live in {address?.ToUpper()}");
+
+
+
+
+
