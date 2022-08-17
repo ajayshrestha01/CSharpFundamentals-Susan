975e3483ba2e670acbf9a71890c9d11486823538
Susan-Sharma <susansharma227@gmail.com>
2022-08-17 22:35:26 +0545

    Declare Age,Name,Gender,Weight

--- CSharpBasics/Program.cs
+++ CSharpBasics/Program.cs
@@ -1,12 +1,37 @@
-﻿Console.Write("What is your number?");
-var numbertext = Console.ReadLine();
-var number = int.Parse(numbertext);
+﻿byte age = 25;
+String Name = "Susan";
+String? Gender = "Female";
+byte Weight = 50;
+Console.WriteLine("Age = {0}", age);
+Console.WriteLine("Name = {0}", Name);
+Console.WriteLine("Gender = {0}", Gender);
+Console.WriteLine("Weight = {0}", Weight);
 
-    if (number% 2 == 0 )
-    Console.WriteLine(number + " is even");
-    else 
-    Console.WriteLine(number + "is odd");
-    
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
