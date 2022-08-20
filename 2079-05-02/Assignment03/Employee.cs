ccb6a6ed9bc1be5fffff8e2d9b7733ec29f100aa
Susan-Sharma <susansharma227@gmail.com>
2022-08-20 20:00:27 +0545

    Creating new employee

---
+++ 2079-05-02/Assignment03/Employee.cs
@@ -0,0 +1,33 @@
+public class Employee
+{
+    public string firstName;
+    public string lastName;
+    public byte employeeID;
+    public bool isMale;
+    public string department;
+    public int hoursWorked;
+    public bool isWorking;
+
+
+    public void work()
+    {
+Console.WriteLine($"{firstName} is working.");
+hoursWorked = hoursWorked + 1;
+Console.WriteLine($" {firstName} has worked for {hoursWorked} hours.");
+    }
+    
+public void stopWorking()
+{
+   Console.WriteLine($"{firstName} will stop working now.");
+    isWorking = false;
+    Console.WriteLine($"is {firstName} working? {isWorking}");
+}
+
+public void startWorking()
+{
+    Console.WriteLine($"{firstName} will start working now.");
+    isWorking = true;
+    Console.WriteLine($"is {firstName} working? {isWorking}");
+}
+
+}
--- 2079-05-02/Assignment03/Program.cs
+++ 2079-05-02/Assignment03/Program.cs
@@ -1,5 +1,16 @@
-﻿
-/*
+﻿var Susan = new Employee();
+Susan.firstName = "Susan";
+Susan.lastName = "Sharma";
+Susan.employeeID = 20;
+Susan.isMale = false;
+Susan.department = "Finance";
 
-assignment goes here..
-*/
+Susan.startWorking();
+
+
+Susan.work();
+
+
+Susan.work();
+
+Susan.stopWorking();
