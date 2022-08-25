21fbd119d6598424d3f21ff9f1f6ae11cd3697c7
Susan-Sharma <susansharma227@gmail.com>
2022-08-25 22:43:49 +0545

    Assignment-Methods

---
+++ 2079-05-09/Appointment.cs
@@ -0,0 +1,28 @@
+class Appointment
+
+
+{
+
+    public string Name(string firstName, string lastName)
+    {
+
+
+        string FullName = $"{firstName} {lastName}";
+        return FullName;
+
+    }
+    public DateTime DisplaySchedule()
+    {
+        return DateTime.Now.AddDays(2);
+        
+
+    }
+
+    public string Name(string firstName, string lastName, string salutation, string degree)
+    {
+        string DoctorDetails = $" {salutation}. {firstName} {lastName}, {degree}";
+        return DoctorDetails;
+
+    }
+
+}
---
+++ 2079-05-09/Program.cs
@@ -0,0 +1,17 @@
+﻿Appointment A = new();
+var FullName = A.Name("Susan", "Sharma");
+Console.WriteLine($"Patient's Name: {FullName}");
+
+var Schedule = A.DisplaySchedule();
+Console.WriteLine($"Appointment Schedule: {Schedule}");
+var DoctorName = A.Name("Saurav", "Dahal", "Dr", "M.S");
+Console.WriteLine($"Doctor's Details:{DoctorName}");
+
+
+
+
+
+
+
+
+
